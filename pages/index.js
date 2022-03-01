4bc92b82fed3c1c82a806f9c41edda7623860f30
youngkwon02 <youngkwon02@naver.com>
2022-03-02 02:21:23 +0900

    feat: apply server side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div>
       <Seo title="Home"></Seo>
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -42,4 +34,16 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+// Below code only run at server-side
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
