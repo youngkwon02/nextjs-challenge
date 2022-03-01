0b6a50e027aa604bdbae457ad875b067d5105dd8
youngkwon02 <youngkwon02@naver.com>
2022-03-02 01:46:50 +0900

    style: popular movie data fetching style

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,44 @@ import Seo from "../components/Seo";
 const API_KEY = "d93891c14273cc26118c7fcfe0f2725b";
 
 export default function Home() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
-      const data = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
-      console.log(data);
+      const { results } = await (await fetch(`/api/movies`)).json();
+      setMovies(results);
     })();
   }, []);
   return (
     <div>
       <Seo title="Home"></Seo>
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
