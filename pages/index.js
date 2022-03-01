becaaeab1fba7565726f97ab72c7b374be95c474
youngkwon02 <youngkwon02@naver.com>
2022-03-02 01:21:09 +0900

    feat: popular movie data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "d93891c14273cc26118c7fcfe0f2725b";
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      console.log(data);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home"></Seo>
